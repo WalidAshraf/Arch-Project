580d2a33c5be3ebfd6dac0da9ee138d4c8618f61
walidashraf <walidashraf423@gmail.com>
2018-12-19 23:59:50 +0200

    Integrated Controller with RomController

--- Controller.vhd
+++ Controller.vhd
@@ -7,19 +7,20 @@ GENERIC(addressBits: integer :=16;
 	registers: integer :=8;
 	wordSize: integer :=16);
 PORT(MAR_A, MAR_B, MAR_C, MDR_C, MDR_B, MDR_A_Out, src_enA, src_enB, dst_en, rst, clk, we, mem_clk, temp_B_Out, temp_enC,temp_A_Out, IR_enC, 
-	IR_A_Out,ALSU_S0,ALSU_S1,ALSU_S2,ALSU_S3,Cin: in std_logic;
-     Cout: OUT STD_LOGIC;
+	IR_A_Out,ALSU_S0,ALSU_S1,ALSU_S2,ALSU_S3,Cin,FALU: in std_logic;
+     CarryFlag,ZeroFlag: OUT STD_LOGIC;
      busA,busB: inout std_logic_vector(wordSize - 1 downto 0);
      busC:INOUT std_logic_vector(wordSize -1 downto 0);
-     src, dst: std_logic_vector(integer(ceil(log2(real(registers))))-1 downto 0));
+     src, dst: IN std_logic_vector(integer(ceil(log2(real(registers))))-1 downto 0));
 END Controller;
 
 ARCHITECTURE MyController OF Controller IS
-SIGNAL MARen, MDRen: std_logic;
+SIGNAL MARen, MDRen,Carry,Cout,NoOpFlag,ParityFlag,OverFlowFlag: std_logic;
 SIGNAL MARbus: std_logic_vector(wordSize - 1 downto 0);
 SIGNAL MAR_Q, MDR_Q, temp_Q, dataout, IR_Q: std_logic_vector(wordSize - 1 downto 0);
 SIGNAL MDR_D, MAR_D: std_logic_vector(addressBits - 1 downto 0);
 SIGNAL Offset: std_logic_vector(wordSize - 1 downto 0);
+SIGNAL ALUSel: STD_LOGIC_VECTOR(3 downto 0);
 BEGIN
 	Offset(wordSize - 1 downto 9) <= (others => '0');
 	Offset(8 downto 0) <= IR_Q(8 downto 0);
@@ -36,10 +37,16 @@ BEGIN
 			     PORT MAP(mem_clk, we, MAR_Q, MDR_Q, dataout);
 	temp: ENTITY work.MyRegister GENERIC MAP(wordSize)
 				     PORT MAP(busC, temp_enC, clk, rst, temp_Q);
+
+	FlagRegisterFile: ENTITY work.FlagRegister PORT MAP(busA,busB,busC,ALUSel,FALU,Cout,Clk,rst,CarryFlag,
+							ZeroFlag,NoOpFlag,ParityFlag,OverFlowFlag);
+
 	IR: ENTITY work.MyRegister GENERIC MAP(wordSize) PORT MAP(busC, IR_enC, clk, rst, IR_Q);
 	IRTri: ENTITY work.tristate GENERIC MAP(wordSize) PORT MAP(Offset, IR_A_Out, busA);
 	TempTriA: ENTITY work.tristate GENERIC MAP(wordSize) PORT MAP(temp_Q, temp_A_Out, busA);
 	TempTriB: ENTITY work.tristate GENERIC MAP(wordSize) PORT MAP(temp_Q, temp_B_Out, busB);
+
+ALUSel <= ALSU_S3&ALSU_S2&ALSU_S1&ALSU_S0;
 MARen <= MAR_A or MAR_B or MAR_C;
 MARbus <= busA when MAR_A = '1' else busB when MAR_B = '1' else busC when MAR_C = '1';
 MDRen <= MDR_B or MDR_C;
--- ROMController.vhd
+++ ROMController.vhd
@@ -16,7 +16,8 @@ Architecture Dataflow of ROMController is
 SIGNAL OneOp, TwoOp, Branch, NoOp, MOV, CMP, cin,PCoutA,RsrcoutA,RdstoutA,MARinA,MARinC,
 	IRoutA,MDRoutA,MDRinB,TEMPoutB,RdstoutB,PCoutB,AplusB,
 	Aminus1,A,Aplus1,ALU,RD,WR,ENDsig,WMFC,RsrcinC,RdstinC,
-	MDRinC,TEMPinC,PCinC,IRinC,RsrcoutB,TEMPoutA,CinRom:STD_LOGIC;
+	MDRinC,TEMPinC,PCinC,IRinC,RsrcoutB,TEMPoutA,CinRom,CarryFlag,ZeroFlag,enA,enB,enC,rstController,clk,we,mem_clk:STD_LOGIC;
+SIGNAL busA,busB,busC: STD_LOGIC_VECTOR(RegWordSize -1 DOWNTO 0);
 SIGNAL ControlWord: std_logic_vector(ROMWordSize - 1 downto 0);  --7asab grouping Walid
 SIGNAL MicroPC: std_logic_vector(ROMAddressBits - 1 downto 0) := "011110";	--3'aleban 6 bits
 SIGNAL NextMicroPC: std_logic_vector(ROMAddressBits - 1 downto 0) := "011110";
@@ -29,10 +30,11 @@ SIGNAL SrcIndirect, DstIndirect: std_logic;
 SIGNAL Result: std_logic;
 SIGNAL Fmap: std_logic_vector (3 downto 0);
 SIGNAL FmapALU: std_logic_vector (3 downto 0);
-SIGNAL BranchType: std_logic_vector (2 downto 0);
+SIGNAL BranchType,Src,Dst: std_logic_vector (2 downto 0);
 SIGNAL AddressBits: std_logic_vector (5 downto 0);
 SIGNAL modeSRC, modeDST: std_logic_vector (1 downto 0);
 SIGNAL b8: std_logic;
+SIGNAL FMappedDecoded: STD_LOGIC_VECTOR(3 DOWNTO 0);
 BEGIN
 	PROCESS
 	BEGIN
@@ -57,16 +59,26 @@ BEGIN
 	DC: ENTITY work.DecodingCircuit PORT MAP(Instruction, OneOp, TwoOp,  --Gets Instruction type as well as MOV and CMP
 						 Branch, NoOp, MOV, CMP, SrcIndirect, DstIndirect); 
 	C: ENTITY work.CinGen PORT MAP(Instruction, Carry, OneOp, TwoOp, cin);	--Osama's IR dependent ALU cin
-	BranchCond: ENTITY work.BranchCondition PORT MAP(Instruction, Carry, Zero, Result);	--Checks if branching condition is met
+	BranchCond: ENTITY work.BranchCondition PORT MAP(Instruction, CarryFlag, ZeroFlag, Result);	--Checks if branching condition is met
 	ROMDecoder: ENTITY work.RomDecoder PORT MAP(ControlWord,PCoutA,RsrcoutA,RdstoutA,MARinA,MARinC, --Decode ROM control word
 						IRoutA,MDRoutA,MDRinB,TEMPoutB,RdstoutB,PCoutB,AplusB,
 						Aminus1,A,Aplus1,ALU,RD,WR,ENDsig,WMFC,RsrcinC,RdstinC,
 						MDRinC,TEMPinC,PCinC,IRinC,RsrcoutB,TEMPoutA,CinRom);
 	FmapEntity: ENTITY work.FMapping PORT MAP(AplusB, Aminus1, A, Aplus1, Fmap);	--Walid's ALU function mapper
 	ALUmap: ENTITY work.FmapperALU PORT MAP(Instruction, TwoOp, OneOp, FmapALU);	--Osama's IR dependent ALU function mapper
+
+	Controller: ENTITY work.Controller GENERIC MAP(16,8,16) PORT MAP(MARinA,'0', MARinC, MDRinC, MDRinB, MDRoutA,enA,enB, 
+	enC,rstController,clk, we, mem_clk, TEMPoutB, TEMPinC,TEMPoutA, IRinC, 
+	IRoutA,FMappedDecoded(0),FMappedDecoded(1),FMappedDecoded(2),FMappedDecoded(3),cinController,ALU,CarryFlag,ZeroFlag,busA,busC,busB,Src,Dst);-- WATCHOUT RST.CLK,WE,MEM_CLK NOT INIT
+	
+	SrcDstDec: ENTITY work.SrcDstDecoder PORT MAP(Instruction,RsrcoutA,RsrcoutB,RsrcinC,RdstoutA,RdstoutB,RdstinC,OneOp,TwoOp,Src,Dst,enA,enB,enC);
+
 	cinController <= CinRom when ALU = '0'
 	ELSE cin;
-	
+
+	FMappedDecoded <= Fmap WHEN ALU ='0'
+	ELSE FmapALU;
+
 	InstructionType <= "00" WHEN TwoOp = '1'
 			   ELSE "01" WHEN OneOp = '1'
 			   ELSE "10" WHEN Branch = '1'
